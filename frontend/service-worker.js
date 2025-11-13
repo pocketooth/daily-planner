f97ca308956d6a937f4e0a5631d7050d4978e7ee
John C. Huang <pocketooth@users.noreply.github.com>
2025-11-13 08:52:56 -0800

    Update service-worker.js to reference icons at root and refresh cache definitions

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -12,8 +12,8 @@ const urlsToCache = [
   '/style.css',
   '/app.js',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
